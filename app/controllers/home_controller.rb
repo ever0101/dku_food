f36b4d9780ac714cd3de32c0d7a8135057bb144d
ever0101 <ever1697@naver.com>
2016-05-16 09:04:16 +0000

    menubar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
     def index
-
+        @post = Post.all
+    end
+    
+    def create
+        @post = Post.new(name: params[:name], phone: params[:phone])
+        @post.save
+        redirect_to :back
     end
 end
